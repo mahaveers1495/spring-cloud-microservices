d0ccab70fdf7e5f16e8e2c946aded1504c8ef3aa
mahaveers1495 <mahaveer.veer95@gmail.com>
2019-07-22 13:27:29 +0530

    sample microservices

---
+++ dicovery/src/main/java/com/mahaveer/microservices/dicovery/DicoveryApplication.java
@@ -0,0 +1,15 @@
+package com.mahaveer.microservices.dicovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DicoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicoveryApplication.class, args);
+	}
+
+}
